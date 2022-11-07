71e8a2d95aecd433288ca1f70b8dd19e3b2f9dcc
kellsnc <kellspam@outlook.fr>
2022-11-07 22:30:50 +0100

    Improve free cursor movement

--- sadx-new-tricks/Big.cpp
+++ sadx-new-tricks/Big.cpp
@@ -10,49 +10,7 @@ static bool EnableFreeCursor = true;
 
 TaskHook BigTheCat_h(0x490A00);
 UsercallFuncVoid(MoveFishingCursor, (task* tp), (tp), 0x46F850, rEAX);
-
-static Float calcFishingCursorY_custom(Float posx, Float posy, Float posz, task* tp)
-{
-	auto pnum = TASKWK_PLAYERID(tp->twp);
-	auto ptwp = playertwp[pnum];
-	auto& ang = tp->twp->value.w[0];
-
-	Angle old_ang = ang;
-	Angle limit = (playerpwp[pnum]->equipment & Upgrades_PowerRod ? 0x4000 : 0x2000);
-
-	if (perG[pnum].on & Buttons_L)
-	{
-		ang = min(+limit, ang + 0x100);
-	}
-	
-	if (perG[pnum].on & Buttons_R)
-	{
-		ang = max(-limit, ang - 0x100);
-	}
-
-	Float new_y = ptwp->pos.y + (njSin(ang) * (tp->awp->work.f[0] / 2));
-
-	posy -= 20.0f;
-	NJS_POINT3 pos = { posx, posy + 2.0f, posz };
-	NJS_POINT3 dir = { 0.0f, new_y - posy, 0.0f };
-
-	if (1 /*!MSetPosition(&pos, &dir, NULL, 1.0f)*/)
-	{
-		return new_y;
-	}
-	else
-	{
-		ang = old_ang;
-		return posy;
-	}
-}
-
-static bool chkCursorLimit_custom(Float pos_x, Float pos_y, Float pos_z, Uint32 pnum)
-{
-	auto ptwp = playertwp[pnum];
-	Float dist = njSqrt((ptwp->pos.x - pos_x) * (ptwp->pos.x - pos_x) + (ptwp->pos.z - pos_z) * (ptwp->pos.z - pos_z));
-	return dist > (playerpwp[pnum]->equipment& Upgrades_PowerRod ? 350.0f : 250.0f);
-}
+UsercallFuncVoid(calcFishingCursorY, (Big_ypos* big_y_ptr, Float posx, Float posy, Float posz, Sint32 flag), (big_y_ptr, posx, posy, posz, flag), 0x46DA00, rESI, stack4, stack4, stack4, stack4);
 
 static void __cdecl MoveFishingCursor_r(task* tp)
 {
@@ -63,40 +21,56 @@ static void __cdecl MoveFishingCursor_r(task* tp)
 
 	auto& input = input_dataG[pnum];
 	
-	NJS_POINT3 v;
-	Angle3 ang;
-	if (input.stroke == 0.0f)
-	{
-		v.x = 0.0f;
-		v.y = 0.0f;
-		v.z = 0.0f;
-		ang = twp->ang; // added
-	}
-	else
+	NJS_POINT3 v{};
+	if (input.stroke != 0.0f)
 	{
 		v.x = njCos(input.angle) * 3.0f;
-		v.y = 0.0f;
 		v.z = njSin(input.angle) * 3.0f;
-		
-		/*MSetPosition(&pos, &v, &ang, 3.0f);*/
 	}
 
-	Float x = twp->pos.x + v.x;
-	Float y = twp->pos.y + v.y;
-	Float z = twp->pos.z + v.z;
-
-	Float h = calcFishingCursorY_custom(x, y, z, tp);
-
-	if (!chkCursorLimit_custom(x, h, z, pnum))
+	if (perG[pnum].on & Buttons_L)
 	{
-		twp->pos.x = x;
-		twp->pos.y = h + 20.0f;
-		twp->pos.z = z;
-
-		awp->work.f[1] = h;
-		awp->work.uw[6] = 0;
-		awp->work.uw[7] = 0;
+		v.y = +3.0f;
 	}
+	else if (perG[pnum].on & Buttons_R)
+	{
+		v.y = -3.0f;
+	}
+
+	NJS_POINT3 pos = { twp->pos.x, twp->pos.y - 15.0f, twp->pos.z };
+	MSetPositionWIgnoreAttribute(&pos, &v, nullptr, ColFlags_Water, 1.0f);
+	pos = { twp->pos.x, twp->pos.y - 20.0f, twp->pos.z };
+	njAddVector(&pos, &v);
+
+	// Limit position
+	NJS_POINT3 center = { ptwp->pos.x - pos.x, ptwp->pos.y - pos.y, ptwp->pos.z - pos.z };
+	Float limit = (playerpwp[pnum]->equipment & Upgrades_PowerRod ? 350.0f : 250.0f);
+	if (njScalor(&center) > limit)
+	{
+		njUnitVector(&center);
+		pos.x = ptwp->pos.x - center.x * limit;
+		pos.y = ptwp->pos.y - center.y * limit;
+		pos.z = ptwp->pos.z - center.z * limit;
+	}
+
+	// Additional height limit
+	Float ylimit = njSin(0x4000) * (tp->awp->work.f[0] / 2);
+	if (pos.y - ptwp->pos.y > ylimit)
+	{
+		pos.y = ptwp->pos.y + ylimit;
+	}
+	if (pos.y - ptwp->pos.y < -ylimit)
+	{
+		pos.y = ptwp->pos.y - ylimit;
+	}
+
+	twp->pos.x = pos.x;
+	twp->pos.y = pos.y + 20.0f;
+	twp->pos.z = pos.z;
+
+	awp->work.f[1] = pos.y;
+	awp->work.uw[6] = 0;
+	awp->work.uw[7] = 0;
 
 	v.x = twp->pos.x - ptwp->pos.x;
 	v.z = twp->pos.z - ptwp->pos.z;
